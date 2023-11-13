96e16935ff6e6e83e9d00c7261ca52bcb434cbc3
nagulapallishashi <144743954+nagulapallishashi@users.noreply.github.com>
2023-11-13 15:15:36 -0500

    Add files via upload

---
+++ Nagulapalli-Shashi_project_progress_report2/admin.php
@@ -0,0 +1,58 @@
+ <?php include("header.php"); 
+ error_reporting(0);
+						     
+							  $msg1=$_GET['msg1'];
+							 if($msg1=='fail')
+							echo "<script type='text/javascript'>alert('Invalid Credentials');</script>";
+
+					
+
+			?>
+			
+
+
+ <section class="inner-page-banner bg_img overlay-3" data-background="assets/images/inner-page-bg.jpg">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="page-title">login</h2>
+          <ol class="page-list">
+            <li><a href="index.html"><i class="fa fa-home"></i> Home</a></li>
+            <li>login</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </section>
+
+     
+
+ <section class="login-section pt-120 pb-120">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="login-block text-center">
+            <div class="login-block-inner">
+              <h3 class="title">Admin Login </h3>
+              <form class="login-form" action="adminloginchk.php">
+                
+                
+                <div class="frm-group">
+                  <input type="text" name="unm" id="f-name" required placeholder="User name">
+                </div>
+                <div class="frm-group">
+                  <input type="password" name="pass" id="pass" required placeholder="Password">
+                </div>
+                <div class="frm-group">
+                  <input type="submit" value="login ">
+                </div>
+              </form>
+              <!-- <p><a href="#0">Haven't your any account in here?</a></p> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+ <?php include("footer.php"); ?>
